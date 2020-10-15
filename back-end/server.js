c16cc80bf24a7f467858cf7caefe8ebf38770573
mudit choudhary <muditchoudhary4@gmail.com>
2020-10-15 21:19:50 +0530

    Phase-2

--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
+import {notFound,errorHandler} from './middleware/middleware.js'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import productRoute from './routes/productRoute.js'
 
 dotenv.config()
+connectDB()
 const app = express()
+
+
 
 app.get('/',(req,res)=>{
     res.send('Api')
 })
-app.get('/api/products',(req,res)=>{
-res.json(products)
-})
-app.get('/api/product/:id',(req,res)=>{
-    const product = products.find((p)=> p._id === req.params.id)
-    res.json(product)
-    })
+app.use('/api/products',productRoute)
+
+app.use(notFound)
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT,console.log(`server ${process.env.NODE_ENV} ${PORT}`))
+app.listen(PORT,console.log(`server ${process.env.NODE_ENV} ${PORT}`.yellow.bold))
